ef9b8b08f19a260e9bff435c52dcb2e35ccf6603
Dean <deanhadas1@gmail.com>
2025-08-10 14:00:17 +0300

    HW_2 Add listeners for the count
    The buttons and text view now have functionality.
    - The first button displays the current count in a toast message.
    - The text view updates to show the current count.
    - The second button increments the count.

--- app/src/main/java/com/example/ex_1_layout/MainActivity.java
+++ app/src/main/java/com/example/ex_1_layout/MainActivity.java
@@ -7,18 +7,47 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.view.View;
 public class MainActivity extends AppCompatActivity {
 
+    private int count = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        initUI();
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+
+    private void initUI(){
+        Button btn_show_message = findViewById(R.id.btn_show_message);
+        Button btn_click = findViewById(R.id.btn_click);
+        TextView textView = findViewById(R.id.textView);
+        btn_click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count++;
+                textView.setText(String.valueOf(count));
+            }
+        });
+
+        btn_show_message.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, ""+count, Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
+
     }
 }
